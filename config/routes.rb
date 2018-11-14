6382aa48be3538b53479fe5b34c1897f60162b2f
Fredis <fredericstojicevic@gmail.com>
2018-11-14 16:44:58 +0100

    Add the structure of the landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'landing_page/index'
+  root 'landing_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
