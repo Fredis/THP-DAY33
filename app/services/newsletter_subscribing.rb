88a334f35e92c754cb21298e73c4efd3070603df
Fredis <fredericstojicevic@gmail.com>
2018-11-15 14:06:18 +0100

    Add params[:p] for target authentication.

--- app/services/newsletter_subscribing.rb
+++ app/services/newsletter_subscribing.rb
@@ -4,7 +4,8 @@ class NewsletterSubscribing
 	def mailchimp_add_a_mail_to_a_list(mail)
 		mailchimp = Mailchimp::API.new(Rails.application.credentials.MAILCHIMP_API_KEY)
 		mailchimp.lists.subscribe(Rails.application.credentials.MAILCHIMP_LIST_ID, 
-      { "email" => mail
+      { :email => mail,
+      	:double_optin => false
       })
 	end
 
