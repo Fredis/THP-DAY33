2bcfb154b56a5f54e5c31dbbd6486c320c6fc8ac
Cedric Soares <cedricsoares@me.com>
2018-11-15 17:27:03 +0100

    twitter_bot service updat

--- app/services/twitter_bot.rb
+++ app/services/twitter_bot.rb
@@ -1,27 +1,27 @@
-
-
 class twitter_bot
 
 
 	def config 
-
 		@client = Twitter::Streaming::Client.new do |config|
   		config.consumer_key        = Rails.application.credentials.dig(:TWITTER_CONFIG_CONSUMER_KEY)
 		config.consumer_secret     = Rails.application.credentials.dig(:TWITTER_CONSUMMER_SECRET)
 		config.access_token        = Rails.application.credentials.dig(:TWITTER_ACCES_TOKEN)
 		config.access_token_secret = Rails.application.credentials.dig(:CONFIG_ACCESS_TOKEN_SECRECT)
+		end
 	end 
 
 	def auto_tweet_cto
 
 	    @client.search("Recherche & CTO", result_type: "recent").take(25).each do |tweet|
         puts @client.update("@#{tweet.user.screen_name} Ne cherchez plus un CTO et apprenez vous même à développer votre application avec le bootcamp The Hacking Project ! Plus d'informations : http://bit.ly/2QKRt39"
+        end
    end
 
    def auto_tweet_formation
 
-	    @tclient.search("Apprendre à coder", result_type: "recent").take(25).each do |tweet|
+	    @client.search("Apprendre à coder", result_type: "recent").take(25).each do |tweet|
         puts @client.update("@#{tweet.user.screen_name} Apprenez à coder en 3 mois avec la formation de the hacking project ! Plus d'informations : http://bit.ly/2QKfVSp"
+        end
    end
  
 	def perfom
