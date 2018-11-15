7e8dbae6049849efcca0089f77a4e6fb1fb1120e
Fredis <fredericstojicevic@gmail.com>
2018-11-15 13:17:46 +0100

    Add Mailchimp API + subscribing OK.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.5.1'
 
 gem 'bootstrap', '~> 4.1.3'
 
+gem 'mailchimp-api', require: 'mailchimp'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use postgresql as the database for Active Record
--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,4 +1,19 @@
 class LandingPageController < ApplicationController
+  
   def index
   end
+
+  def subscribing
+  	render "index"
+  	subscriber_mail = newsletter_params[:mail]
+  	service_subscribing = NewsletterSubscribing.new
+  	service_subscribing.perform(subscriber_mail)
+  end
+
+  private
+
+  def newsletter_params
+  	params.require(:newsletter).permit(:mail)
+  end
+
 end
