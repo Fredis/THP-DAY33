5379092b9042e2ce6d89cf566dae86eac1469fa7
Cedric Soares <cedricsoares@me.com>
2018-11-15 15:12:01 +0100

    twitter_bot.service update

--- app/services/twitter_bot.rb
+++ app/services/twitter_bot.rb
@@ -5,7 +5,7 @@ class twitter_bot
 
 	def config 
 
-		client = Twitter::Streaming::Client.new do |config|
+		@client = Twitter::Streaming::Client.new do |config|
   		config.consumer_key        = Rails.application.credentials.dig(:TWITTER_CONFIG_CONSUMER_KEY)
 		config.consumer_secret     = Rails.application.credentials.dig(:TWITTER_CONSUMMER_SECRET)
 		config.access_token        = Rails.application.credentials.dig(:TWITTER_ACCES_TOKEN)
@@ -14,14 +14,14 @@ class twitter_bot
 
 	def auto_tweet_cto
 
-	    @twitter_instance.search("Recherche CTO", result_type: "recent").take(25).each do |tweet|
-        puts @client.update("Ne cherchez plus un CTO et apprenez vous même à développer votre application avec le bootcamp The Hacking Project ! Plus d'informations : https://thp-nantes-hacking-pro.herokuapp.com/", in_reply_to_tweet_id:tweet.user.id)
+	    @client.search("Recherche & CTO", result_type: "recent").take(25).each do |tweet|
+        puts @client.update("@#{tweet.user.screen_name} Ne cherchez plus un CTO et apprenez vous même à développer votre application avec le bootcamp The Hacking Project ! Plus d'informations : https://thp-nantes-hacking-pro.herokuapp.com/"
    end
 
    def auto_tweet_formation
 
-	    @twitter_instance.search("Apprendre à coder", result_type: "recent").take(25).each do |tweet|
-        puts @client.update("Apprenez à coder en 3 mois avec la formation de the hacking project ! Plus d'informations : https://thp-nantes-hacking-pro.herokuapp.com/", in_reply_to_tweet_id:tweet.user.id)
+	    @tclient.search("Apprendre à coder", result_type: "recent").take(25).each do |tweet|
+        puts @client.update("@#{tweet.user.screen_name} Apprenez à coder en 3 mois avec la formation de the hacking project ! Plus d'informations : https://thp-nantes-hacking-pro.herokuapp.com/"
    end
  
 	def perfom
