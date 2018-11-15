ca94511aa1e7dc96702c2e3452946f7e9d1d19e8
Fredis <fredericstojicevic@gmail.com>
2018-11-15 20:24:37 +0100

    Add good redirections after validationg the form.

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -7,16 +7,17 @@ class LandingPageController < ApplicationController
   end
 
   def subscribing
+    @subscriber_source = newsletter_params[:page]
   	render "index"
   	subscriber_mail = newsletter_params[:mail]
   	service_subscribing = NewsletterSubscribing.new
-  	service_subscribing.perform(subscriber_mail)
+  	service_subscribing.perform(subscriber_mail, @subscriber_source)
   end
 
   private
 
   def newsletter_params
-  	params.require(:newsletter).permit(:mail)
+  	params.require(:newsletter).permit(:mail, :page)
   end
 
 end
